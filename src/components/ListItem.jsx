b4841c330a8ab9f3bf6c2d8845afa906c566bbbb
M0n00s <programacion.buffalo@gmail.com>
2022-07-29 10:33:01 -0400

    add hover effect and delete

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteInverText } from '../redux/slices/iEcho/iEchoSlice'
+import './css/ListItem.css'
 
 export const ListItem = ({ itext = [] }) => {
+  const dispatch = useDispatch()
+
+  const handleDelete = (id) => {
+    dispatch( deleteInverText(id)) 
+  }
   return (
     <ul className='list-group'>
       {
             itext.map((inverText) => (
-              <li key={inverText.text} className='list-group-item d-flex justify-content-between'>
+              <li 
+              onClick={ () => handleDelete(inverText.id) } 
+              key={inverText.id} 
+              className='list-group-item d-flex justify-content-between listItem__item'              
+              
+              >
                 <span className='align-self-center'>{inverText.text}</span>
                 {
                   inverText.palindrome && <span className='badge text-bg-success'>¡is Palindrome!</span>
--- src/redux/slices/iEcho/iEchoSlice.js
+++ src/redux/slices/iEcho/iEchoSlice.js
@@ -16,9 +16,12 @@ export const iEchoSlice = createSlice({
     setInverText: (state, action) => {
       state.isLoading = false
       state.inverText = [action.payload, ...state.inverText]
+    },
+    deleteInverText: (state, action) => {
+      state.inverText = state.inverText.filter(text => text.id !== action.payload)
     }
 
   }
 })
 
-export const { startInverText, setInverText } = iEchoSlice.actions
+export const { startInverText, setInverText, deleteInverText } = iEchoSlice.actions
